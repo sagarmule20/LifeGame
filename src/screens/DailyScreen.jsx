e2cd1e5a1619d968eb25495631fcaf4f283e836e
SAGARMULE\sagar.mule <sagar.mule@agenatrader.com>
2026-04-13 09:37:00 +0200

    Revert "fix: show total earned as balance, remove today balance from header"
    This reverts commit 98e28ca6fb1b533dfab73d0abaf85fa525bacdf0.

--- src/screens/DailyScreen.jsx
+++ src/screens/DailyScreen.jsx
@@ -34,8 +34,9 @@ export default function DailyScreen() {
           <p className="text-xs font-bold" style={{ color: 'var(--text-dim)' }}>{today}</p>
         </div>
         <div className="text-right">
+          <p className="text-xl font-black" style={{ color: '#FFC800' }}>💶 €{euros}</p>
           <p className="text-[10px] font-bold" style={{ color: rank.color }}>
-            {rank.icon} {rank.name}
+            You are {rank.icon} {rank.name}
           </p>
         </div>
       </div>
